a4f55eae675d2cee4907e115a79a915a48e9cd5c
Mark Johnson <mark@twistersdomain.co.uk>
2023-08-07 17:26:18 +0000

    Copilot third commit

---
+++ comments.js
@@ -0,0 +1,18 @@
+// Create Web Server
+var express = require('express');
+var app = express();
+
+// Create a route to handle incoming requests
+app.get('/', function(req, res){
+    res.send('Hello World!');
+});
+
+// Create a route to handle incoming requests
+app.get('/comments', function(req, res){
+    res.send('Comments!');
+});
+
+// Start the server
+app.listen(3000, function(){
+    console.log('Server is running on Port 3000');
+});
