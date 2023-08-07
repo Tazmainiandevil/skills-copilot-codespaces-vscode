f3794164fd9f6658c4bf2e8170d6e691731f8e06
Mark Johnson <mark@twistersdomain.co.uk>
2023-08-07 17:49:04 +0000

    Copilot third commit

--- comments.js
+++ comments.js
@@ -1,18 +1,71 @@
 // Create Web Server
 var express = require('express');
 var app = express();
+var bodyParser = require('body-parser');
+var mongoose = require('mongoose');
+mongoose.connect('mongodb://localhost:27017/test');
+var db = mongoose.connection;
 
-// Create a route to handle incoming requests
-app.get('/', function(req, res){
-    res.send('Hello World!');
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
+
+// Create Schema
+var commentSchema = mongoose.Schema({
+    name: String,
+    comment: String,
+    time: String
 });
 
-// Create a route to handle incoming requests
-app.get('/comments', function(req, res){
-    res.send('Comments!');
+// Create Model
+var Comment = mongoose.model('Comment', commentSchema);
+
+// Create Router
+app.get('/api/comments', function(req, res) {
+    Comment.find(function(err, comments) {
+        if (err) return res.status(500).send({error: 'database failure'});
+        res.json(comments);
+    })
 });
 
-// Start the server
-app.listen(3000, function(){
-    console.log('Server is running on Port 3000');
-}); 
+app.get('/api/comments/:comment_id', function(req, res) {
+    Comment.findOne({_id: req.params.comment_id}, function(err, comment) {
+        if (err) return res.status(500).json({error: err});
+        if (!comment) return res.status(404).json({error: 'comment not found'});
+        res.json(comment);
+    })
+});
+
+app.post('/api/comments', function(req, res) {
+    var comment = new Comment();
+    comment.name = req.body.name;
+    comment.comment = req.body.comment;
+    comment.time = req.body.time;
+
+    comment.save(function(err) {
+        if (err) {
+            console.error(err);
+            res.json({result: 0});
+            return;
+        }
+        res.json({result: 1});
+    });
+});
+
+app.put('/api/comments/:comment_id', function(req, res) {
+    Comment.findById(req.params.comment_id, function(err, comment) {
+        if (err) return res.status(500).json({error: 'database failure'});
+        if (!comment) return res.status(404).json({error: 'comment not found'});
+
+        if (req.body.name) comment.name = req.body.name;
+        if (req.body.comment) comment.comment = req.body.comment;
+        if (req.body.time) comment.time = req.body.time;
+
+        comment.save(function(err) {
+            if (err) res.status(500).json({error: 'failed to update'});
+            res.json({message: 'comment updated'});
+        });
+    });
+});
+
+app.delete('/api/comments/:comment_id', function(req, res) {
+    Comment.remove({_id: req.params.comment_id}, function(err, output
